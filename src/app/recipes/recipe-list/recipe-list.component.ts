0e1096e1d3eb02e367d49e525a1be70a45fa1eb1
Marcin <marcinpokusinski@gmail.com>
2022-03-24 22:51:35 +0100

    add temp recipe

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe} from "./recipe.model";
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,6 +7,9 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+  recipes: Recipe[] = [
+    new Recipe("Pizza", "Make pizza with great cheese", "https://cdn.galleries.smcloud.net/t/galleries/gf-Khjk-FUGy-Lsd8_domowa-pizza-na-szybko-wedlug-karola-okrasy-latwy-przepis-na-wielka-wyzerke-1920x1080-nocrop.jpg"),
+  ];
 
   constructor() { }
 
